00439722dcd98fae14984cd22feb95e319334260
ls1955 <xxlsxx1955@gmail.com>
2023-08-20 21:13:35 +0800

    Include project.js

---
+++ src/project.js
@@ -0,0 +1,8 @@
+export const Project = {
+    new: function(name = "Default project") {
+        const project = Object.create(this);
+
+        project.name = name;
+        project.todos = [];
+    }
+};
