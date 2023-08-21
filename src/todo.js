7563636f7b3e419db3eba55daf976effbd3a547c
ls1955 <xxlsxx1955@gmail.com>
2023-08-21 20:50:03 +0800

    Remove default arguments
    Because:
    All arguments is probably more appriopriate to being
    pass explicitly.
    
    This commit also:
    Include dueDate argument(as it is mentioned
    inside the project).

--- src/todo.js
+++ src/todo.js
@@ -1,10 +1,12 @@
 export const Todo = {
-    new: function(title = "Default title", description = "Default description", priority = "normal") {
+    new: function(title, description, dueDate, priority) {
         const todo = Object.create(this);
 
         todo.title = title;
         todo.description = description;
+        todo.dueDate = dueDate;
         todo.priority = priority;
+        todo.isCompleted = false;
 
         return todo;
     }
