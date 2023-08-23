8a690c4c2882e24cab2aed39738a1e794df8bd69
ls1955 <xxlsxx1955@gmail.com>
2023-08-23 08:08:32 +0800

    Include project buttons in sidebar
    Because:
    The project "tabs" should be clickable, and
    the main content could easily be change according
    via click event.
    
    This commit also:
    Include more projects and todos for now.

--- src/webpage-controller.js
+++ src/webpage-controller.js
@@ -1,12 +1,23 @@
+import { Todo } from "./todo.js";
 import { Project } from "./project.js";
 
 export const WebpageController = {
     new: function() {
         const controller = Object.create(this);
 
-        let defaultProject = Project.new();
+        let defaultProject = Project.new("Default Project");
+        let projectOne = Project.new("The Odin Project");
+        let projectTwo = Project.new("The Euler Project");
+        let todoOne = Todo.new("Eat breakfast", "Eat some breakfast to begin the day", Date.now(), 3);
+        let todoTwo = Todo.new("Study", "Study", Date.now(), 3);
+        let todoThree = Todo.new("Complete this project", "Complete this beautiful project.", Date.now(), 1);
+        defaultProject.push(todoOne);
+        defaultProject.push(todoTwo);
+        projectOne.push(todoThree);
 
-        controller.projects = [defaultProject];
+        defaultProject.updateMainContent();
+
+        controller.projects = [defaultProject, projectOne, projectTwo];
 
         return controller;
     },
@@ -16,15 +27,12 @@ export const WebpageController = {
         sidebar.innerHTML = "";
 
         this.projects.forEach(project => {
-            // let projectButton = document.createElement("button");
-            let projectDiv = document.createElement("div");
+            let projectButton = document.createElement("button");
 
-            // projectButton.innerText = project.name;
-            // projectButton.addEventListener("onclick", project.updateMainContent);
-            projectDiv.innerText = project.name;
+            projectButton.innerText = project.name;
+            projectButton.addEventListener("click", () => project.updateMainContent());
 
-            // sidebar.appendChild(projectDiv.toDOM());
-            sidebar.appendChild(projectDiv);
+            sidebar.appendChild(projectButton);
         });
     }
 };
