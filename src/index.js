2a6bda619996d5fd75671c4142d3f8357bd4a542
ls1955 <xxlsxx1955@gmail.com>
2023-08-23 08:18:27 +0800

    Include TODOs

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import { WebpageController } from "./webpage-controller.js";
 
 let controller = WebpageController.new();
 controller.initPage();
+
+// TODO:
+// Include index? in projectButtons
+// Include a form/dialog to create new todo
+// Include a button to create new project
