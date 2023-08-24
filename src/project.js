dfafb5ea12893a8b1773321bcaa42347e85695bb
ls1955 <xxlsxx1955@gmail.com>
2023-08-24 20:52:54 +0800

    Update current project index
    This commit:
    * Include project.index attribute
    * Include button's click event to update index
      in .current-project-index-holder

--- src/project.js
+++ src/project.js
@@ -13,8 +13,11 @@ export const Project = {
     updateMainContent: function() {
         const main = document.querySelector(".todo-list-container");
         const ul = document.createElement("ul");
+        const indexHolder = document.querySelector(".current-project-index-holder");
 
         main.innerHTML = "";
+        // Let others know which project should new todo insert into
+        indexHolder.dataset.currentIndex = this.index;
 
         this.todos.forEach(todo => {
             const li = document.createElement("li");
@@ -30,7 +33,12 @@ export const Project = {
         const button = document.createElement("button");
 
         button.innerText = this.name;
-        button.addEventListener("click", () => this.updateMainContent());
+
+        button.addEventListener("click", () => {
+            // Button's dataset index will be assign by sidebarController
+            this.index = button.dataset.index;
+            this.updateMainContent();
+        });
 
         return button;
     }
