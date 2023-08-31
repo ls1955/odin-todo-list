3d8e9f850f0252e9b1631f53e49b73ce5030f377
ls1955 <xxlsxx1955@gmail.com>
2023-08-31 21:27:29 +0800

    Include edit-container event
    This commit:
    * Include checkbox button's event to toggle
      edit-container
    * Initialize edit-container textContent
    * Include inputs's event to update information
      immediately

--- src/todo.js
+++ src/todo.js
@@ -18,12 +18,53 @@ export const Todo = {
         const template = document.querySelector("#todo-template");
         const result = template.content.cloneNode(true);
 
-        result.querySelector(".todo-title").textContent = this.title;
+        const titleOutput = result.querySelector(".todo-title");
+        const descriptionOutput = result.querySelector(".description");
+        const dueDateOutput = result.querySelector(".due-date");
+        const priorityOutput = result.querySelector(".priority");
 
-        result.querySelector(".description").innerText = this.description;
-        result.querySelector(".due-date").innerText = this.dueDate;
-        result.querySelector(".priority").innerText = this.priority;
-        
+        titleOutput.textContent = this.title;
+        descriptionOutput.textContent = this.description;
+        dueDateOutput.textContent = this.dueDate;
+        priorityOutput.textContent = this.priority;
+
+        const editBtn = result.querySelector(".edit-button");
+        const editContainer = result.querySelector(".edit-container");
+        editBtn.addEventListener("click", () => {
+            editContainer.style.display = editContainer.style.display === "block" ?
+                                          "none" :
+                                          "block";
+        });
+
+        const titleEditInput = editContainer.querySelector(".title-edit-input");
+        const descriptionEditInput = editContainer.querySelector(".description-edit-input");
+        const dueDateEditInput = editContainer.querySelector(".due-date-edit-input");
+        const priorityEditInput = editContainer.querySelector(".priority-edit-input");
+
+        titleEditInput.value = this.title;
+        descriptionEditInput.value = this.description;
+        // Commented out for now as author doesn't feel like working with date for now
+        // dueDate.value = this.dueDate;
+        priorityEditInput.value = this.priority;
+
+        titleEditInput.addEventListener("input", () => {
+            this.title = titleEditInput.value;
+            titleOutput.textContent = this.title;
+        });
+
+        descriptionEditInput.addEventListener("input", () => {
+            this.description = descriptionEditInput.value;
+            descriptionOutput.textContent = this.description;
+        });
+        dueDateEditInput.addEventListener("input", () => {
+            // this.dueDate = dueDateEditInput.value
+            // dueDateOutput.value = this.dueDate;
+        });
+        priorityEditInput.addEventListener("input", () => {
+            this.priority = priorityEditInput.value
+            priorityOutput.value = this.priority;
+        });
+
         const detailsContainer = result.querySelector(".details-container");
         const toggleDetailsBtn = result.querySelector(".toggle-details-button");
         toggleDetailsBtn.addEventListener("click", () => {
