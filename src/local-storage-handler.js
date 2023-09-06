f7b7e4649998119df1002b1135230850a891b186
ls1955 <xxlsxx1955@gmail.com>
2023-09-06 20:49:25 +0800

    Implement #init
    Because:
    This function might become the new
    entry point of the program, where
    it will check the localStorage
    and act accordingly.
    
    This commit also:
    * Implement ::new
    * Implement #buildDefaultProject
    * Implement #buildFromLocalStorage

--- src/local-storage-handler.js
+++ src/local-storage-handler.js
@@ -1,14 +1,25 @@
-// Looks like the handler should in charge
-// of managing all the projects and todos
-// in the page (It has to update and modify
-// local storage, afterall.)
+import { DialogController } from "./dialog-controller";
+import { Project } from "./project";
+import { SidebarController } from "./sidebar-controller";
+import { Todo } from "./todo";
+
 export const LocalStorageHandler = {
+    new: function() {
+        const handler = Object.create(this);
+
+        handler.projects = [];
+
+        return handler;
+    },
     init: function() {
-        // Check if projects already inside local storage
-        // if exists?:
-        // load the projects
-        // else:
-        // load some predefined project
+        if (localStorage.getItem("odin-todo-list-projects") != null) {
+            this.buildFromLocalStorage();
+        } else {
+            this.buildDefaultProject();
+        }
+
+        DialogController.new(this.projects);
+        SidebarController.new(this.projects).populateSidebar();
     },
     push: function(project) {
         // push the project json into localstorage
@@ -16,8 +27,33 @@ export const LocalStorageHandler = {
     update: function(project) {
         // find the project in localstorage, then update it
     },
-    build: function(projectJSON) {
-        // perhaps a helper function that build particular
-        // project (and its todo)
+    buildDefaultProject: function() {
+        let today = new Date().toISOString().substring(0, 10);
+
+        let defaultTodoOne = Todo.new("Sleep", "Go to bed early tonight.", today, 3);
+        let defaultTodoTwo = Todo.new("Live healthy", "Be kind to myself", today, 3);
+        let defaultProject = Project.new("Life", [defaultTodoOne, defaultTodoTwo]);
+
+        localStorage.setItem(
+            "odin-todo-list-projects",
+            JSON.stringify([defaultProject.toJSON()])
+        );
+        this.projects.push(defaultProject);
+    },
+    buildFromLocalStorage() {
+        JSON.parse(localStorage.getItem("odin-todo-list-projects")).forEach(projectData => {
+            let project = Project.new(projectData["name"]);
+
+            projectData["todos"].forEach(todoData => {
+                let todo = Todo.new(
+                    todoData["title"],
+                    todoData["description"],
+                    todoData["dueDate"],
+                    todoData["priority"]
+                )
+                project.push(todo);
+            });
+            this.projects.push(project);
+        });
     }
 };
