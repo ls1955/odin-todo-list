c805893943aed4db31b92017489a40b21344e745
ls1955 <xxlsxx1955@gmail.com>
2023-09-06 20:52:47 +0800

    Remove bunch of stuff
    This commit:
    Remove default projects and controllers
    setup statements (as those are now being handle
    by LocalStorageHandler).
    
    This commit also:
    Include a safety seatbelt for clicking button
    event, in case the button could not be found for
    some reason.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
-import { DialogController } from "./dialog-controller.js";
-import { Project } from "./project.js";
-import { SidebarController } from "./sidebar-controller.js";
-import { Todo } from "./todo.js";
+import { LocalStorageHandler } from "./local-storage-handler";
 
 import "./style.css";
 
-let today = new Date().toISOString().substring(0, 10);
-
-let todoOne = Todo.new("Eat breakfast", "Eat some breakfast to begin the day", today, 3);
-let todoTwo = Todo.new("Study", "Study", today, 3);
-let todoThree = Todo.new("Complete this project", "Complete this beautiful project.", today, 1);
-
-let defaultProject = Project.new("Default Project", [todoOne, todoTwo]);
-let projectOne = Project.new("The Odin Project", [todoThree]);
-let projectTwo = Project.new("The Euler Project");
-
-let projects = [defaultProject, projectOne, projectTwo];
-
-let sidebarController = SidebarController.new(projects);
-let dialogController = DialogController.new(projects);
-
-sidebarController.populateSidebar();
+LocalStorageHandler.new().init();
 
 // Select and click first project button to update main
 // content and update current project index
-document.querySelector(".project-button").click();
+document.querySelector(".project-button")?.click();
 
 // Optional?:
 // For every project, it should be nice to show them
